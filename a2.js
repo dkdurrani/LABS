99bea30825cfdf1570619050f7d4a3a651dcede0
dkdurrani <92779285+dkdurrani@users.noreply.github.com>
2023-06-04 13:34:14 +1000

    a2.js

---
+++ a2.js
@@ -0,0 +1,33 @@
+const collegeData = require('./collegeData');
+
+collegeData.initialize()
+  .then(() => {
+    console.log('Successfully initialized.');
+
+    collegeData.getAllStudents()
+      .then(students => {
+        console.log(`Successfully retrieved ${students.length} students`);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    collegeData.getCourses()
+      .then(courses => {
+        console.log(`Successfully retrieved ${courses.length} courses`);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    collegeData.getTAs()
+      .then(tas => {
+        console.log(`Successfully retrieved ${tas.length} TAs`);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  })
+  .catch(error => {
+    console.error(error);
+  });
